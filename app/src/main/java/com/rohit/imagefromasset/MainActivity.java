3005a12ac58301d737a3d81254d6c61e72a9dca1
Rohit Kumar <rohit9075@gmail.com>
2018-07-30 15:59:58 +0530

    new comments

--- app/src/main/java/com/rohit/imagefromasset/MainActivity.java
+++ app/src/main/java/com/rohit/imagefromasset/MainActivity.java
@@ -2,13 +2,12 @@ package com.rohit.imagefromasset;
 
 import android.content.res.AssetManager;
 import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,16 +23,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // instantiating the objects
         mImageView = findViewById(R.id.imageView);
         mButton = findViewById(R.id.button);
 
 
+        // handling the button click event
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                AssetManager assetManager = getAssets();
+
+                // getting the image from the asset and showing it into the imageView
+
                 try {
+                    AssetManager assetManager = getAssets();
                     InputStream ims = assetManager.open("ic_background.png");
                     Drawable d = Drawable.createFromStream(ims, null);
                     mImageView.setImageDrawable(d);
